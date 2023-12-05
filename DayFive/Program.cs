56e92247787511491ca6032fae8a69fbdfcf0802
Kevin Lee <kevinlee2@corelogic.com>
2023-12-05 11:41:33 +0000

    day 5 wip

---
+++ DayFive/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var lines = await File.ReadAllLinesAsync($"{AppDomain.CurrentDomain.BaseDirectory}\\input.txt");
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+var linkedList = new LinkedList<int>();
+
+foreach (var line in lines)
+{
+    if (line.StartsWith("seeds:"))
+    {
+        var seeds = NumberRegex().Matches(line);
+    }
+}
+
+partial class Program
+{
+    [GeneratedRegex("\\d+")]
+    private static partial Regex NumberRegex();
+}
